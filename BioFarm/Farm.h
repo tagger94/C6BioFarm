b094d32819496bd5bda23f7243ad5c76c4cb6c72
Alex Jeffery Berns <tagger94@12macpro07.design.iastate.edu>
2014-10-04 10:40:49 -0500

    go

---
+++ BioFarm/Farm.h
@@ -0,0 +1,15 @@
+//
+//  Farm.h
+//  BioFarm
+//
+//  Created by Alex Jeffery Berns on 10/4/14.
+//  Copyright (c) 2014 Iowa State. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Farm : NSObject
+
+
+
+@end
---
+++ BioFarm/Farm.m
@@ -0,0 +1,13 @@
+//
+//  Farm.m
+//  BioFarm
+//
+//  Created by Alex Jeffery Berns on 10/4/14.
+//  Copyright (c) 2014 Iowa State. All rights reserved.
+//
+
+#import "Farm.h"
+
+@implementation Farm
+
+@end
