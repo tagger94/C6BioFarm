ac1aa5ae0cf6cad5fe5d5982a669974a34089e28
Alex Jeffery Berns <tagger94@12macpro07.design.iastate.edu>
2014-10-04 09:33:02 -0500

    Initial Commit

---
+++ BioFarm/BioAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BioAppDelegate.h
+//  BioFarm
+//
+//  Created by Alex Jeffery Berns on 10/4/14.
+//  Copyright (c) 2014 Iowa State. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BioAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BioFarm/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BioFarm
+//
+//  Created by Alex Jeffery Berns on 10/4/14.
+//  Copyright (c) 2014 Iowa State. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BioAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BioAppDelegate class]));
+    }
+}
