8e73aa7142265cbbe020ec0e0c719d110ce47de4
tagger94 <tagger94@12macpro07.design.iastate.edu>
2014-10-04 14:13:40 -0500

    Added farm.m

---
+++ BioFarm/Farm.m
@@ -0,0 +1,29 @@
+//
+//  Farm.m
+//  BioFarm
+//
+//  Created by Alex Jeffery Berns on 10/4/14.
+//  Copyright (c) 2014 Iowa State. All rights reserved.
+//
+
+#import "Farm.h"
+
+@implementation Farm
+
+Land farmland[20][50];
+
++ (void) createFarmLand {
+    
+    
+    for (int x = 0; x < 20; x++) {
+        for (int y = 0; y < 50; y++) {
+            [farmland[x][y] tillLand];
+        }
+    }
+}
+
++ (Land[][]) getFarmLand {
+    return farmland;
+}
+
+@end
